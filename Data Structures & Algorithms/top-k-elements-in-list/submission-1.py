1ead900427e86893e466f05cae5c1c8d62a0bd6e
Thiago Roberto <thiagosroberto.2013@gmail.com>
2026-08-11 20:03:40 -0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Etapa 1
+        # criaria um dicionário para contar:
+        #   chave: num; valor: qtde
+        dicionario = {}
+
+        # Etapa 2
+        # validação de chave -> Verificar se o elemento existe ou não.
+        #   existe: +1; não existe: criar a chave + 1  # .get()
+        for num in nums:
+            dicionario[num] = dicionario.get(num, 0) + 1
+
+        # Etapa 3
+        # ordenar o dicionário pelos valores em ordem decrescente
+        # separar os top k valores.
+        # .items() --> [(chave,valor),(num, qntd),...]
+        nums_s = sorted(dicionario.items(),key = lambda x: x[1], reverse = True)
+        # criar lista topk
+
+        topk = [i[0] for i in nums_s]
+
+
+        # Etapa fim 
+        return topk[0:k]
